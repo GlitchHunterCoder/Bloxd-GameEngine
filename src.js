2100eeb1dd14de8757f541b8d82851f9691ab49e
GlitchHunterCoder <209635473+GlitchHunterCoder@users.noreply.github.com>
2026-05-13 16:02:56 +0100

    Update src.js

--- src.js
+++ src.js
@@ -331,152 +331,123 @@ function order() {
   return result
 }
 
-function compileConstructor(dtype) {
-  var className = "View3d"+dtype
-
-  var useGetters = (dtype === "generic")
-
-  var code = ["'use strict'"]
-
-  //Create constructor for view
-  var indices = [0,1,2]
-  var args = ["i0","i1","i2"]
-  var index_str = "this.offset + this.stride[0]*i0 + this.stride[1]*i1 + this.stride[2]*i2"
-      
-  var shapeArg = "b0,b1,b2"
-  var strideArg = "c0,c1,c2"
-  code.push(
-    "function "+className+"(a,b0,b1,b2,c0,c1,c2,d){this.data=a",
-      "this.shape=[b0,b1,b2]",
-      "this.stride=[c0,c1,c2]",
-      "this.offset=d|0}",
-    "var proto="+className+".prototype",
-    "proto.dtype='"+dtype+"'",
-    "proto.dimension=3")
-
-  //view.size:
-  code.push("Object.defineProperty(proto,'size',{get:function "+className+"_size(){\
-return this.shape[0]*this.shape[1]*this.shape[2]",
-"}})")
-
-  code.push("Object.defineProperty(proto,'order',{get:")
-
-  code.push("function "+className+"_order(){")
-
-  code.push(
-"var s0=Math.abs(this.stride[0]),s1=Math.abs(this.stride[1]),s2=Math.abs(this.stride[2]);\
-if(s0>s1){\
-if(s1>s2){\
-return [2,1,0];\
-}else if(s0>s2){\
-return [1,2,0];\
-}else{\
-return [1,0,2];\
-}\
-}else if(s0>s2){\
-return [2,0,1];\
-}else if(s2>s1){\
-return [0,1,2];\
-}else{\
-return [0,2,1];\
-}}})")
-
-  //view.set(i0, ..., v):
-  code.push(
-"proto.set=function "+className+"_set(i0,i1,i2,v){")
-  if(useGetters) {
-    code.push("return this.data.set(this.offset + this.stride[0]*i0 + this.stride[1]*i1 + this.stride[2]*i2,v)}")
-  } else {
-    code.push("return this.data[this.offset + this.stride[0]*i0 + this.stride[1]*i1 + this.stride[2]*i2]=v}")
+function Compiled(CTOR_LIST, ORDER){
+  'use strict'
+  
+  function View3dChunk(a, b0, b1, b2, c0, c1, c2, d) {
+    this.data = a
+    this.shape = [b0, b1, b2]
+    this.stride = [c0, c1, c2]
+    this.offset = d | 0
   }
-
-  //view.get(i0, ...):
-  code.push("proto.get=function "+className+"_get(i0,i1,i2){")
-  if(useGetters) {
-    code.push("return this.data.get(this.offset + this.stride[0]*i0 + this.stride[1]*i1 + this.stride[2]*i2)}")
-  } else {
-    code.push("return this.data[this.offset + this.stride[0]*i0 + this.stride[1]*i1 + this.stride[2]*i2]}")
+  var proto = View3dChunk.prototype
+  proto.dtype = 'Chunk'
+  proto.dimension = 3
+  Object.defineProperty(proto, 'size', {
+    get: function View3dChunk_size() {
+      return this.shape[0] * this.shape[1] * this.shape[2]
+    }
+  })
+  Object.defineProperty(proto, 'order', {
+    get: function View3dChunk_order() {
+      return [0,1,2].sort((a,b) => Math.abs(this.stride[a]) - Math.abs(this.stride[b]))
+    }
+  })
+  proto.set = function View3dChunk_set(i0, i1, i2, v) {
+    return this.data[this.offset + this.stride[0] * i0 + this.stride[1] * i1 + this.stride[2] * i2] = v
   }
-
-  //view.index:
-  code.push(
-    "proto.index=function "+className+"_index(", "i0,i1,i2", "){return this.offset + this.stride[0]*i0 + this.stride[1]*i1 + this.stride[2]*i2}")
-
-  //view.hi():
-  code.push("proto.hi=function "+className+"_hi(i0,i1,i2){return new "+className+"(this.data,(typeof i0!=='number'||i0<0)?this.shape[0]:i0|0,(typeof i1!=='number'||i1<0)?this.shape[1]:i1|0,(typeof i2!=='number'||i2<0)?this.shape[2]:i2|0,this.stride[0],this.stride[1],this.stride[2],this.offset)}")
-    
-
-  //view.lo():
-  var a_vars = ["a0=this.shape[0]","a1=this.shape[1]","a2=this.shape[2]"]
-  
-  
-  var c_vars = ["c0=this.stride[0]","c1=this.stride[1]","c2=this.stride[2]"]
-  
-  
-  code.push("proto.lo=function "+className+"_lo(i0,i1,i2){var b=this.offset,d=0,a0=this.shape[0],a1=this.shape[1],a2=this.shape[2],c0=this.stride[0],c1=this.stride[1],c2=this.stride[2]")
-
-code.push(
-"if(typeof i0==='number'&&i0>=0){d=i0|0;b+=c0*d;a0-=d}"
-)
-code.push(
-"if(typeof i1==='number'&&i1>=0){d=i1|0;b+=c1*d;a1-=d}"
-)
-code.push(
-"if(typeof i2==='number'&&i2>=0){d=i2|0;b+=c2*d;a2-=d}"
-)
-  
-  code.push("return new "+className+"(this.data,a0,a1,a2,c0,c1,c2,b)}")
-    
-    
-
-  //view.step():
-  code.push("proto.step=function "+className+"_step(i0,i1,i2){var a0=this.shape[0],a1=this.shape[1],a2=this.shape[2],b0=this.stride[0],b1=this.stride[1],b2=this.stride[2],c=this.offset,d=0,ceil=Math.ceil")
-    
-    
-    
-code.push(
-"if(typeof i0==='number'){d=i0|0;if(d<0){c+=b0*(a0-1);a0=ceil(-a0/d)}else{a0=ceil(a0/d)}b0*=d}"
-)
-code.push(
-"if(typeof i1==='number'){d=i1|0;if(d<0){c+=b1*(a1-1);a1=ceil(-a1/d)}else{a1=ceil(a1/d)}b1*=d}"
-)
-code.push(
-"if(typeof i2==='number'){d=i2|0;if(d<0){c+=b2*(a2-1);a2=ceil(-a2/d)}else{a2=ceil(a2/d)}b2*=d}"
-)
-
-  code.push("return new "+className+"(this.data,a0,a1,a2,b0,b1,b2,c)}")
-
-  //view.transpose():
-  var tShape = new Array(3)
-  var tStride = new Array(3)
-  for(var i=0; i<3; ++i) {
-    tShape[i] = "a[i"+i+"]"
-    tStride[i] = "b[i"+i+"]"
+  proto.get = function View3dChunk_get(i0, i1, i2) {
+    return this.data[this.offset + this.stride[0] * i0 + this.stride[1] * i1 + this.stride[2] * i2]
   }
-  code.push("proto.transpose=function "+className+"_transpose("+args+"){"+
-    args.map(function(n,idx) { return n + "=(" + n + "===undefined?" + idx + ":" + n + "|0)"}).join(";"),
-    "var a=this.shape,b=this.stride;return new "+className+"(this.data,"+tShape.join(",")+","+tStride.join(",")+",this.offset)}")
-
-  //view.pick():
-  code.push("proto.pick=function "+className+"_pick("+args+"){var a=[],b=[],c=this.offset")
-  for(var i=0; i<3; ++i) {
-    code.push("if(typeof i"+i+"==='number'&&i"+i+">=0){c=(c+this.stride["+i+"]*i"+i+")|0}else{a.push(this.shape["+i+"]);b.push(this.stride["+i+"])}")
+  proto.index = function View3dChunk_index(i0, i1, i2){
+    return this.offset + this.stride[0] * i0 + this.stride[1] * i1 + this.stride[2] * i2
   }
-  code.push("var ctor=CTOR_LIST[a.length+1];return ctor(this.data,a,b,c)}")
-
-  //Add return statement
-  code.push("return function construct_"+className+"(data,shape,stride,offset){return new "+className+"(data,"+
-    indices.map(function(i) {
-      return "shape["+i+"]"
-    }).join(",")+","+
-    indices.map(function(i) {
-      return "stride["+i+"]"
-    }).join(",")+",offset)}")
-
-  //Compile procedure
-  var procedure = new Function("CTOR_LIST", "ORDER", code.join("\n"))
-  return procedure(CACHED_CONSTRUCTORS[dtype], order)
-}
+  proto.hi = function View3dChunk_hi(i0, i1, i2) {
+    return new View3dChunk(this.data,
+    (typeof i0 !== 'number' || i0 < 0) ? this.shape[0] : i0 | 0,
+    (typeof i1 !== 'number' || i1 < 0) ? this.shape[1] : i1 | 0,
+    (typeof i2 !== 'number' || i2 < 0) ? this.shape[2] : i2 | 0,
+    this.stride[0], this.stride[1], this.stride[2], this.offset)
+  }
+  proto.lo = function View3dChunk_lo(i0, i1, i2) {
+    var [a0,a1,a2] = [...this.shape]
+    var [c0,c1,c2] = [...this.stride]
+    var b = this.offset, d = 0;
+    if(typeof i0==='number'&&i0>=0){d=i0|0;b+=c0*d;a0-=d}
+    if(typeof i1==='number'&&i1>=0){d=i1|0;b+=c1*d;a1-=d}
+    if(typeof i2==='number'&&i2>=0){d=i2|0;b+=c2*d;a2-=d}
+    return new View3dChunk(this.data, a0, a1, a2, c0, c1, c2, b)
+  }
+  proto.step = function View3dChunk_step(i0, i1, i2) {
+    var [a0,a1,a2] = [...this.shape]
+    var [b0,b1,b2] = [...this.stride] 
+    var c = this.offset, d = 0, ceil = Math.ceil
+    if (typeof i0 === 'number') {
+      d = i0 | 0;
+      if (d < 0) {
+        c += b0 * (a0 - 1);
+        a0 = ceil(-a0 / d)
+      } else {
+        a0 = ceil(a0 / d)
+      }
+      b0 *= d
+    }
+    if (typeof i1 === 'number') {
+      d = i1 | 0;
+      if (d < 0) {
+        c += b1 * (a1 - 1);
+        a1 = ceil(-a1 / d)
+      } else {
+        a1 = ceil(a1 / d)
+      }
+      b1 *= d
+    }
+    if (typeof i2 === 'number') {
+      d = i2 | 0;
+      if (d < 0) {
+        c += b2 * (a2 - 1);
+        a2 = ceil(-a2 / d)
+      } else {
+        a2 = ceil(a2 / d)
+      }
+      b2 *= d
+    }
+    return new View3dChunk(this.data, a0, a1, a2, b0, b1, b2, c)
+  }
+  proto.transpose = function View3dChunk_transpose(i0, i1, i2) {
+    i0 = (i0 === undefined ? 0 : i0 | 0);
+    i1 = (i1 === undefined ? 1 : i1 | 0);
+    i2 = (i2 === undefined ? 2 : i2 | 0)
+    var a = this.shape, b = this.stride;
+    return new View3dChunk(this.data, a[i0], a[i1], a[i2], b[i0], b[i1], b[i2], this.offset)
+  }
+  proto.pick = function View3dChunk_pick(i0, i1, i2) {
+    var a = [], b = [], c = this.offset
+    if (typeof i0 === 'number' && i0 >= 0) {
+      c = (c + this.stride[0] * i0) | 0
+    } else {
+      a.push(this.shape[0]);
+      b.push(this.stride[0])
+    }
+    if (typeof i1 === 'number' && i1 >= 0) {
+      c = (c + this.stride[1] * i1) | 0
+    } else {
+      a.push(this.shape[1]);
+      b.push(this.stride[1])
+    }
+    if (typeof i2 === 'number' && i2 >= 0) {
+      c = (c + this.stride[2] * i2) | 0
+    } else {
+      a.push(this.shape[2]);
+      b.push(this.stride[2])
+    }
+    var ctor = CTOR_LIST[a.length + 1];
+    return ctor(this.data, a, b, c)
+  }
+  return function construct_View3dChunk(data, shape, stride, offset) {
+    return new View3dChunk(data, ...shape, ...stride, offset)
+  }
+}  
 
 function arrayDType(data) {
   if(!!data?.constructor?.isBuffer?.(data)) return "buffer"
@@ -487,7 +458,6 @@ function arrayDType(data) {
     if(type in CACHED_CONSTRUCTORS) return type
   }
   if(Array.isArray(data)) return "array"
-  return "generic"
 }
 
 var CACHED_CONSTRUCTORS = {
@@ -503,7 +473,6 @@ var CACHED_CONSTRUCTORS = {
   "bigint64": [],
   "biguint64": [],
   "buffer":[],
-  "generic":[]
 }
 
 function wrappedNDArrayCtor(data, shape, stride, offset) {
@@ -535,7 +504,7 @@ function wrappedNDArrayCtor(data, shape, stride, offset) {
   var dtype = arrayDType(data)
   var ctor_list = CACHED_CONSTRUCTORS[dtype]
   while(ctor_list.length <= d+1) {
-    ctor_list.push(compileConstructor(dtype))
+    ctor_list.push(Compiled(CACHED_CONSTRUCTORS[dtype], order))
   }
   var ctor = ctor_list[d+1]
   return ctor(data, shape, stride, offset)
