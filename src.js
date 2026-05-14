4d51a0b89e0240220aa324e8a0764f14d0731db5
GlitchHunterCoder <209635473+GlitchHunterCoder@users.noreply.github.com>
2026-05-14 13:16:41 +0100

    Update src.js

--- src.js
+++ src.js
@@ -590,4 +590,356 @@ Player = class {
   }
 }
 
+//ndarray FULL modern refactor
+
+function order() {
+  var stride = this.stride
+  var terms = new Array(stride.length)
+  var i
+  for(i=0; i<terms.length; ++i) {
+    terms[i] = [Math.abs(stride[i]), i]
+  }
+  terms.sort((a,b)=>a[0]-b[0])
+  var result = new Array(terms.length)
+  for(i=0; i<result.length; ++i) {
+    result[i] = terms[i][1]
+  }
+  return result
+}
+
+function compileConstructor(dtype, dimension) {
+  var className = ["View", dimension, "d", dtype].join("")
+  if(dimension < 0) {
+    className = "View_Nil" + dtype
+  }
+  var useGetters = (dtype === "generic")
+
+  if(dimension === -1) {
+    //Special case for trivial arrays
+  
+    let fn = {
+      [className]:function(a){this.data=a},
+      ["construct_"+className]:function(a){return new fn[className](a)}
+    }
+    let proto=fn[className].prototype;
+    proto.dtype=dtype;
+    proto.index=function(){return -1};
+    proto.size=0;
+    proto.dimension=-1;
+    proto.shape=proto.stride=proto.order=[];
+    proto.lo=proto.hi=proto.transpose=proto.step=function(){return new fn[className](this.data);};
+    proto.get=proto.set=function(){};
+    proto.pick=function(){return null};
+    return fn["construct_"+className]
+
+  } else if(dimension === 0) {
+    //Special case for 0d arrays
+
+    let TrivialArray = CACHED_CONSTRUCTORS[dtype][0]
+    let fn = {
+      [className]:function(a,d){this.data=a;this.offset=d},
+      [className+"_copy"]:function(){return new fn[className](this.data,this.offset)},
+      [className+"_pick"]:function(){return TrivialArray(this.data)},
+      [className+"_get"]:useGetters ? function(){return this.data.get(this.offset)} : function(){return this.data[this.offset]},
+      [className+"_set"]:useGetters ? function(v){return this.data.set(this.offset,v)} : function(v){return this.data[this.offset]=v},
+      ["construct_"+className]:function(a,b,c,d){return new fn[className](a,d)}
+    }
+    
+    var proto=fn[className].prototype;
+    proto.dtype=dtype;
+    proto.index=function(){return this.offset};
+    proto.dimension=0;
+    proto.size=1;
+    proto.shape=proto.stride=proto.order=[];
+    proto.lo=proto.hi=proto.transpose=proto.step=fn[className+"_copy"]
+    proto.pick=fn[className+"_pick"]
+    proto.valueOf=proto.get=fn[className+"_get"]
+    proto.set=fn[className+"_set"]
+    return fn["construct_"+className]
+  }
+
+  var code = []
+
+  //Create constructor for view
+  var indices = Array(dimension).fill(1).map((_,i)=>i)
+  var args = indices.map((e)=>"i"+i) //[10,i1,12]
+  var index_str = "this.offset+" + indices.map((e)=>{
+        return "this.stride[" + i + "]*i" + i
+      }).join("+")
+  var shapeArg = indices.map(function(i) {
+      return "b"+i
+    }).join(",")
+  var strideArg = indices.map(function(i) {
+      return "c"+i
+    }).join(",")
+    
+  procedure = function()(
+    
+`` //START
+
+  let CTOR_LIST = CACHED_CONSTRUCTORS[dtype], ORDER = order
+
+'use strict'
+
+let fn = {
+  [className]:function(a,"+shapeArg+","+strideArg+",d){this.data=a
+    this.shape=["+shapeArg+"]
+    this.stride=["+strideArg+"]
+    this.offset=d|0
+  },
+  [className+"_size"]:function(){
+    return "+indices.map(function(i) { return "this.shape["+i+"]" }).join("*")
+  },
+  [className+"_set"]:useGetters?function("+args.join(",")+"){
+    return this.data.set("+index_str+",v)
+  }:function("+args.join(",")+"){
+    return this.data["+index_str+"]=v
+  },
+  [className+"_get"]:useGetters?function("+args.join(",")+"){
+    return this.data.get("+index_str+")
+  }:function("+args.join(",")+"){
+    return this.data["+index_str+"]
+  },
+  [className+"_index"]:function("+args.join()+"){
+    return "+index_str+"
+  },
+  [className+"_hi"]:function("+args.join(",")+"){
+    return new fn[className](this.data,`
+  //view.hi():
+    indices.map(function(i) {
+      return ["(typeof i",i,"!=='number'||i",i,"<0)?this.shape[", i, "]:i", i,"|0"].join("")
+    }).join(",")+","+
+    indices.map(function(i) {
+      return "this.stride["+i + "]"
+    }).join(",")+",this.offset)}")
+    `)
+  },
+  [className+"_lo"]:function("+args.join(",")+"){`
+
+  //view.lo():
+  var a_vars = indices.map(function(i) { return "a"+i+"=this.shape["+i+"]" })
+  var c_vars = indices.map(function(i) { return "c"+i+"=this.stride["+i+"]" })
+  code.push("proto.lo=function "+className+"_lo("+args.join(",")+"){var b=this.offset,d=0,"+a_vars.join(",")+","+c_vars.join(","))
+  for(var i=0; i<dimension; ++i) {
+    code.push(
+"if(typeof i"+i+"==='number'&&i"+i+">=0){\
+d=i"+i+"|0;\
+b+=c"+i+"*d;\
+a"+i+"-=d}")
+  }
+  code.push("return new "+className+"(this.data,"+
+    indices.map(function(i) {
+      return "a"+i
+    }).join(",")+","+
+    indices.map(function(i) {
+      return "c"+i
+    }).join(",")+",b)}")
+    
+  `},
+  [className+"_step"]:function("+args.join(",")+"){var ` 
+  
+  //view.step():
+  code.push("proto.step=function "+className+"_step("+args.join(",")+"){var "+
+    indices.map(function(i) {
+      return "a"+i+"=this.shape["+i+"]"
+    }).join(",")+","+
+    indices.map(function(i) {
+      return "b"+i+"=this.stride["+i+"]"
+    }).join(",")+",c=this.offset,d=0,ceil=Math.ceil")
+  for(var i=0; i<dimension; ++i) {
+    code.push(
+"if(typeof i"+i+"==='number'){\
+d=i"+i+"|0;\
+if(d<0){\
+c+=b"+i+"*(a"+i+"-1);\
+a"+i+"=ceil(-a"+i+"/d)\
+}else{\
+a"+i+"=ceil(a"+i+"/d)\
+}\
+b"+i+"*=d\
+}")
+  }
+  code.push("return new "+className+"(this.data,"+
+    indices.map(function(i) {
+      return "a" + i
+    }).join(",")+","+
+    indices.map(function(i) {
+      return "b" + i
+    }).join(",")+",c)}")
+    
+  `},
+  [className+"_transpose"]:function(){`
+    
+  //view.transpose():
+  var tShape = new Array(dimension)
+  var tStride = new Array(dimension)
+  for(var i=0; i<dimension; ++i) {
+    tShape[i] = "a[i"+i+"]"
+    tStride[i] = "b[i"+i+"]"
+  }
+  code.push("proto.transpose=function "+className+"_transpose("+args+"){"+
+    args.map(function(n,idx) { return n + "=(" + n + "===undefined?" + idx + ":" + n + "|0)"}).join(";"),
+    "var a=this.shape,b=this.stride;return new "+className+"(this.data,"+tShape.join(",")+","+tStride.join(",")+",this.offset)}")
+    
+  `},
+  [className+"_pick"]:function(){`
+    
+  //view.pick():
+  code.push("proto.pick=function "+className+"_pick("+args+"){var a=[],b=[],c=this.offset")
+  for(var i=0; i<dimension; ++i) {
+    code.push("if(typeof i"+i+"==='number'&&i"+i+">=0){c=(c+this.stride["+i+"]*i"+i+")|0}else{a.push(this.shape["+i+"]);b.push(this.stride["+i+"])}")
+  }
+  code.push("var ctor=CTOR_LIST[a.length+1];return ctor(this.data,a,b,c)}")
+    
+  `},
+  ["construct_"+className]:function(){`
+    
+  //Add return statement
+  code.push("return function construct_"+className+"(data,shape,stride,offset){return new "+className+"(data,"+
+    indices.map(function(i) {
+      return "shape["+i+"]"
+    }).join(",")+","+
+    indices.map(function(i) {
+      return "stride["+i+"]"
+    }).join(",")+",offset)}")
+    
+  `}
+  
+
+
+}
+
+var proto=fn[className].prototype,
+proto.dtype='"+dtype+"',
+proto.dimension="+dimension,
+
+Object.defineProperty(proto,'size',{get:fn[className+"_size"]})
+
+Object.defineProperty(proto,'order',{get:ORDER})
+
+proto.set=fn[className+"_set"]
+
+proto.get=fn[className+"_get"]
+
+proto.index=fn[className+"_index"]
+
+proto.hi=fn[className+"_hi"]
+
+proto.lo=fn[className+"_lo"]
+
+proto.step=fn[className+"_step"]
+
+proto.transpose=fn[className+"_transpose"]
+
+proto.pick=fn[className+"_pick"]
+
+return fn["construct_"+className]
+
+`` //END
+
+
+)
+
+  return procedure()
+}
+
+function arrayDType(data) {
+  if(data?.constructor?.isBuffer?.(data)) {
+    return "buffer"
+  }
+  if((typeof Float64Array) !== "undefined") {
+    switch(Object.prototype.toString.call(data)) {
+      case "[object Float64Array]":
+        return "float64"
+      case "[object Float32Array]":
+        return "float32"
+      case "[object Int8Array]":
+        return "int8"
+      case "[object Int16Array]":
+        return "int16"
+      case "[object Int32Array]":
+        return "int32"
+      case "[object Uint8Array]":
+        return "uint8"
+      case "[object Uint16Array]":
+        return "uint16"
+      case "[object Uint32Array]":
+        return "uint32"
+      case "[object Uint8ClampedArray]":
+        return "uint8_clamped"
+      case "[object BigInt64Array]":
+        return "bigint64"
+      case "[object BigUint64Array]":
+        return "biguint64"
+    }
+  }
+  if(Array.isArray(data)) {
+    return "array"
+  }
+  return "generic"
+}
+
+var CACHED_CONSTRUCTORS = {
+  "float32":[],
+  "float64":[],
+  "int8":[],
+  "int16":[],
+  "int32":[],
+  "uint8":[],
+  "uint16":[],
+  "uint32":[],
+  "array":[],
+  "uint8_clamped":[],
+  "bigint64": [],
+  "biguint64": [],
+  "buffer":[],
+  "generic":[]
+}
+
+;(function() {
+  for(var id in CACHED_CONSTRUCTORS) {
+    CACHED_CONSTRUCTORS[id].push(compileConstructor(id, -1))
+  }
+});
+
+function wrappedNDArrayCtor(data, shape, stride, offset) {
+  if(data === undefined) {
+    var ctor = CACHED_CONSTRUCTORS.array[0]
+    return ctor([])
+  } else if(typeof data === "number") {
+    data = [data]
+  }
+  if(shape === undefined) {
+    shape = [ data.length ]
+  }
+  var d = shape.length
+  if(stride === undefined) {
+    stride = new Array(d)
+    for(var i=d-1, sz=1; i>=0; --i) {
+      stride[i] = sz
+      sz *= shape[i]
+    }
+  }
+  if(offset === undefined) {
+    offset = 0
+    for(var i=0; i<d; ++i) {
+      if(stride[i] < 0) {
+        offset -= (shape[i]-1)*stride[i]
+      }
+    }
+  }
+  var dtype = arrayDType(data)
+  var ctor_list = CACHED_CONSTRUCTORS[dtype]
+  while(ctor_list.length <= d+1) {
+    ctor_list.push(compileConstructor(dtype, ctor_list.length-1))
+  }
+  var ctor = ctor_list[d+1]
+  return ctor(data, shape, stride, offset)
+}
+
+module.exports = wrappedNDArrayCtor
+
+
+
+
 
